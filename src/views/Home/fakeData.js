8ae256f1e4fcc7174d7dfc97c74e47b922f799e2
a5a8a0a9 <a5a8a0a9@yahoo.com.tw>
2021-05-06 00:13:23 +0800

    0506

--- src/views/Home/fakeData.js
+++ src/views/Home/fakeData.js
@@ -1,77 +1,4 @@
-export const doneEnum = {
-  0: "",
-  1: "complete",
-  2: "reject",
-  3: "coding",
-  4: "suck my dick",
-};
-export const doneOptions = [
-  { text: "", value: 0 },
-  { text: "complete", value: 1 },
-  { text: "reject", value: 2 },
-  { text: "coding", value: 3 },
-  { text: "suck my dick", value: 4 },
-];
-
-export const RuleTableHeader = [
-  {
-    text: "名稱",
-    value: "name",
-  },
-  {
-    text: "rule_type",
-    value: "rule_type",
-  },
-  {
-    text: "owner",
-    value: "owner",
-  },
-  {
-    text: "editor",
-    value: "editor",
-  },
-  {
-    text: "done",
-    value: "done",
-  },
-  {
-    text: "qa",
-    value: "qa",
-  },
-  {
-    text: "review",
-    value: "review",
-  },
-  {
-    text: "sc_qa",
-    value: "sc_qa",
-  },
-  {
-    text: "note",
-    value: "note",
-  },
-];
-
-// export const FakeRuleData = [
-//   {
-//     id: 1,
-//     revision: 1,
-//     name: "rule1",
-//     description: "description1",
-//     rule_type: "rule_type1",
-//     owner: "owner1",
-//     editor: "editor1",
-//     done: 0,
-//     qa_editor: "qa_editor1",
-//     qa: 0,
-//     reviewer: "reviewer1",
-//     review: 0,
-//     sc_qa_editor: "sc_qa_editor1",
-//     sc_qa: 0,
-//     note: "note1",
-//     update_date: new Date(),
-//   },
-// ];
+import { doneEnum } from "./Columns";
 export const FakeRuleData = Array.from({ length: 10 }, (v, i) => {
   return {
     id: i + 1,
@@ -93,3 +20,24 @@ export const FakeRuleData = Array.from({ length: 10 }, (v, i) => {
     update_date: new Date(),
   };
 });
+
+// export const FakeRuleData = [
+//   {
+//     id: 1,
+//     revision: 1,
+//     name: "rule1",
+//     description: "description1",
+//     rule_type: "rule_type1",
+//     owner: "owner1",
+//     editor: "editor1",
+//     done: 0,
+//     qa_editor: "qa_editor1",
+//     qa: 0,
+//     reviewer: "reviewer1",
+//     review: 0,
+//     sc_qa_editor: "sc_qa_editor1",
+//     sc_qa: 0,
+//     note: "note1",
+//     update_date: new Date(),
+//   },
+// ];
